8c18839d4318ca2585da8023e602accd00d1b19e
= <scott.frees@gmail.com>
2015-10-29 16:33:21 -0400

    syntax error

--- addon/primesieve.cpp
+++ addon/primesieve.cpp
@@ -7,7 +7,7 @@ using namespace v8;
 void GeneratePrimes(const v8::FunctionCallbackInfo<v8::Value>& args) {
   Isolate* isolate = args.GetIsolate();
   Local<Number> retval = v8::Number::New(isolate, 0);
-  args.GetReturnValue().Set(retval)
+  args.GetReturnValue().Set(retval);
 }
 
 
