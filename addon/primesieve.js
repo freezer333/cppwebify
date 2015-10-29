6281b4ddbb6913249037ba89caf560de67be3323
Scott Frees <scott.frees@gmail.com>
2015-10-29 19:23:07 -0400

    reorganization for npm setup

--- addon/primesieve.js
+++
@@ -1,4 +0,0 @@
-var prime = require("./build/Release/primesieve.node");
-console.log("Calculating Primes");
-prime.generate_primes();
-console.log("Done");
---
+++ basic_node/primesieve.js
@@ -0,0 +1,4 @@
+var prime = require("primesieve4node");
+console.log("Calculating Primes");
+prime.generate_primes();
+console.log("Done");
