0bba864fecc606594ae51427c9202086a1a6c4b7
Scott Frees <scott.frees@gmail.com>
2015-11-27 12:16:43 -0500

    changed exec to async

--- web/routes/standalone_args.js
+++ web/routes/standalone_args.js
@@ -10,31 +10,35 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
 
-    // I'm using sync mode here, but if you don't want to
+    // I'm using exec, but if you don't want to
     // buffer all the output, but use it incrementally, 
-    // you'd use the asynchronous methods (ie. exectFile).
-    // That would be far more effective if you were
+    // you'd use the other methods (ie. streaming).
+    // That would be more effective if you were
     // streaming the results to the browser, but here
     // we need all the primes to render the response anyway...
-    var exec = require('child_process').execFileSync
+    var exec = require('child_process').execFile
     var program = "../cpp/standalone_stdio/build/Release/standalone";
 
 
     var under = parseInt(req.body.under);
-    var output = exec(program, [under]).toString();
-
-    var primes = output.split("\n")
+    var output = exec(program, [under],
+      function (error, stdout, stderr) {
+        var primes = stdout.split("\n")
                        .slice(0, -3)
                        .map(function (line) {
                            return parseInt(line);
                        });
 
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
-      results: primes
-    }));
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({
+          results: primes
+        }));
 
-    console.log("Primes generated using " + type);
+        console.log("Primes generated from " + type);
+    });
+
+
+    
 });
 
   
